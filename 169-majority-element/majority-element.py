8b7f8f7be2a5d9679c009a621f14b3c66ae0c041
Venkat teja Raavi <venkatteja.ravi@gmail.com>
2024-02-12 09:50:51 -0500

    Time: 193 ms (9.01%) | Memory: 18.2 MB (48.67%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -2,10 +2,7 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         hmap = {}
         for i in nums:
-            if str(i) in hmap.keys():
-                hmap[str(i)] += 1
-            else:
-                hmap[str(i)] = 1
+            hmap[str(i)] = hmap.get(str(i), 0) + 1
             if hmap[str(i)] > len(nums)/2:
                     return i
         return -1
