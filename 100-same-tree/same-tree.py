01bb6be211bf13523e586d0c5d56bfd6df681286
Venkat Teja Ravi <venkatteja.ravi@gmail.com>
2024-02-27 18:13:59 -0500

    Time: 30 ms (88.28%) | Memory: 16.7 MB (34.97%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,19 +6,38 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # DFS (Recursion Technique)
-        # Base case
         if p == None and q == None:
             return True
-
-        if (p == None and q != None) or (p != None and q == None):
+        if p == None or q == None:
             return False
 
-        if p.val != q.val:
-            return False
+        # BFS using queue
+        p_que = collections.deque()
+        q_que = collections.deque()
 
-        # Recursion
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        p_que.append(p)
+        q_que.append(q)
+
+        while len(p_que) > 0 and len(q_que) > 0:
+            node1 = p_que.popleft()
+            node2 = q_que.popleft()
+
+            if node1.val != node2.val:
+                return False
+            
+            if node1.left != None and node2.left != None:
+                p_que.append(node1.left)
+                q_que.append(node2.left)
+            elif node1.left != None or node2.left != None:
+                return False
+
+            if node1.right != None and node2.right != None:
+                p_que.append(node1.right)
+                q_que.append(node2.right)
+            elif node1.right != None or node2.right != None:
+                return False
+
+        return True
 
 
         
