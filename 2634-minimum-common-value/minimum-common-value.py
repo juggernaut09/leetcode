fc10cbca3c68d6d5dd990063b2a39580a89ec5aa
Venkat Teja Ravi <venkatteja.ravi@gmail.com>
2024-03-10 00:02:37 -0500

    Time: 340 ms (86.18%) | Memory: 34.8 MB (99.33%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,9 +1,11 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
         i, j = 0,0
-
+        common = float('inf')
         while i < len(nums1) and j < len(nums2):
-            if nums1[i] == nums2[j]: return nums1[i]
+            if nums1[i] == nums2[j]:
+                common = nums1[i]
+                break 
             elif nums1[i] < nums2[j]: i+=1
             else: j+=1
-        return -1
+        return -1 if common == float('inf') else common
