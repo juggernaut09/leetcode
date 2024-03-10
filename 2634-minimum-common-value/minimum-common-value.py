4ca51fbe5e8a22b56994609d64789fb65be0dbfe
Venkat Teja Ravi <venkatteja.ravi@gmail.com>
2024-03-10 00:00:39 -0500

    Time: 357 ms (39.92%) | Memory: 35.6 MB (73.36%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,11 +1,9 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        hmap = {}
+        i, j = 0,0
 
-        for num in nums1:
-            hmap[num] = hmap.get(num, 0) + 1
-
-        for num in nums2:
-            if num in hmap.keys():
-                return num
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]: return nums1[i]
+            elif nums1[i] < nums2[j]: i+=1
+            else: j+=1
         return -1
