df50e724397e250f14058aca4614d4f248a255d6
Venkat Teja Ravi <venkatteja.ravi@gmail.com>
2024-04-14 19:03:47 -0400

    Time: 30 ms (90.66%) | Memory: 16.8 MB (8.53%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/sum-of-left-leaves.py
@@ -5,18 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def sumOfLeftLeaves(self, root: TreeNode) -> int:
-        def dfs(node, is_left):
-            if not node:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        def solve(curr: Optional[TreeNode], isLeft: bool):
+            if not curr:
                 return 0
-            if not node.left and not node.right:  # Leaf node
-                if is_left:
-                    return node.val
+            if (not curr.left) and (not curr.right):
+                if isLeft:
+                    return curr.val
                 else:
                     return 0
-            left_sum = dfs(node.left, True)   # Traverse left child
-            right_sum = dfs(node.right, False)  # Traverse right child
-            return left_sum + right_sum
-        
-        return dfs(root, False)
-              
+            
+            left = solve(curr.left, True)
+            right = solve(curr.right, False)
+
+            return left + right
+
+        return solve(root, False)
