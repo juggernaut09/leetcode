03fe953e9acc55c025dcaa5bfea7678b6f2eea4e
Venkat Teja Ravi <venkatteja.ravi@gmail.com>
2024-06-07 05:20:49 +0530

    Time: 481 ms (14.02%) | Memory: 18.5 MB (51.03%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,25 +1,15 @@
-class Solution(object):
-    def find_successors(self, hand, groupSize, i, n):
-        next_val = hand[i] + 1
-        hand[i] = -1  # Mark as used
-        count = 1
-        i += 1
-        while i < n and count < groupSize:
-            if hand[i] == next_val:
-                next_val = hand[i] + 1
-                hand[i] = -1
-                count += 1
-            i += 1
-        return count == groupSize
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        card_counts = {}
+        for card in hand:
+            card_counts[card] = card_counts.get(card, 0) + 1
 
-    def isNStraightHand(self, hand, groupSize):
-        n = len(hand)
-        if n % groupSize != 0:
-            return False
-        hand.sort()
-        for i in range(n):
-            if hand[i] >= 0:
-                if not self.find_successors(hand, groupSize, i, n):
+        while card_counts:
+            first_card = min(card_counts.keys())
+            for i in range(first_card, first_card + groupSize):
+                if not i in card_counts:
                     return False
+                card_counts[i] -= 1
+                if not card_counts[i]:
+                    del card_counts[i]
         return True
-        
