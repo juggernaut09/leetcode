28163a67b0b512ff4bf95d935888cd73433a05f5
Venkat Teja Ravi <venkatteja.ravi@gmail.com>
2024-06-15 04:51:09 +0530

    Time: 637 ms (63.69%) | Memory: 30.2 MB (89.78%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
@@ -1,15 +1,9 @@
 class Solution:
     def minIncrementForUnique(self, nums: List[int]) -> int:
-        # nums = [3,2,1,2,1,7]
-        #        [1,1,2,2,3,7]
-
-        # mySet = set({ num for num in nums }), 2+4
         nums.sort()
-        numTracker = 0
-        minIncreament = 0
-
-        for num in nums:
-            numTracker = max(numTracker, num)
-            minIncreament += numTracker - num
-            numTracker += 1
-        return minIncreament
+        moves = 0
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i-1]:
+                moves += (nums[i-1] - nums[i]) + 1
+                nums[i] = nums[i-1] + 1
+        return moves
