ee7e37262639be33a169ea8d844ea9dc3422982e
Venkat Teja Ravi <venkatteja.ravi@gmail.com>
2024-11-25 01:12:46 -0500

    Time: 34 ms (52.12%) | Memory: 19.3 MB (90.83%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,11 +1,17 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        hmap = {0:1}
-        curr_sum, i, res = 0, 0, 0
-        while i < len(nums):
-            curr_sum += nums[i]
-            if curr_sum - k in hmap.keys():
-                res += hmap[curr_sum - k]
-            hmap[curr_sum] = hmap.get(curr_sum, 0) + 1
-            i += 1
+        res = 0
+        prefix_sum = 0
+        mp = {
+            0:1
+        }
+
+        for num in nums:
+            prefix_sum += num
+
+            if prefix_sum - k in mp:
+                res += mp[prefix_sum - k]
+
+            
+            mp[prefix_sum] = mp.get(prefix_sum, 0) + 1
         return res
