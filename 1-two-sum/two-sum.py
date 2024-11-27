eb3b369fd143faad8ddedfd31ccab4c490d7a733
Venkat Teja Ravi <venkatteja.ravi@gmail.com>
2024-11-26 23:17:00 -0500

    Time: 7 ms (37.68%) | Memory: 19.1 MB (5.31%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,29 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hmap = {}
+        # hmap = {}
+        # for i in range(len(nums)):
+        #     required = target - nums[i]
+        #     if required in hmap:
+        #         return [hmap[required], i]
+        #     else:
+        #         hmap[nums[i]] = i
 
-        for i in range(len(nums)):
-            required = target - nums[i]
-            if required in hmap:
-                return [hmap[required], i]
+        # return []
+        nums = [(val, idx) for idx, val in enumerate(nums)]
+        nums.sort()
+        
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            _sum = nums[left][0] + nums[right][0]
+            if  _sum == target:
+                return [nums[left][1], nums[right][1]]
+            elif _sum > target:
+                right -= 1
             else:
-                hmap[nums[i]] = i
+                left += 1
+
+        return [] 
+
 
         return []
-        
