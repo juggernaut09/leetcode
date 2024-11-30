44ca60514fd774ef6098b8006ad1a347bd2943b7
Venkat Teja Ravi <venkatteja.ravi@gmail.com>
2024-11-29 22:33:34 -0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (6.57%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,5 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        hmap = {num : i for i, num in enumerate(nums)}
+        left = 0
+        right = len(nums) - 1
 
-        return hmap[target] if target in hmap else -1
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            # left sorted array
+            if nums[left] <= nums[mid]:
+                if target > nums[mid] or target < nums[left]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+            else:
+                # right sorted array
+                if target < nums[mid] or target > nums[right]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
