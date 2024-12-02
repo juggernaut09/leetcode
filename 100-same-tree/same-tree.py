e64ec8581d571aa061e93250c93eb4f85cc2faba
Venkat Teja Ravi <venkatteja.ravi@gmail.com>
2024-12-02 15:47:53 -0600

    Time: 0 ms (100.00%) | Memory: 17.3 MB (5.19%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,38 +6,12 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p == None and q == None:
+        if not p and not q:
             return True
-        if p == None or q == None:
+        if not p or not q:
+            return False
+        if p.val != q.val:
             return False
 
-        # BFS using queue
-        p_que = collections.deque()
-        q_que = collections.deque()
-
-        p_que.append(p)
-        q_que.append(q)
-
-        while len(p_que) > 0 and len(q_que) > 0:
-            node1 = p_que.popleft()
-            node2 = q_que.popleft()
-
-            if node1.val != node2.val:
-                return False
-            
-            if node1.left != None and node2.left != None:
-                p_que.append(node1.left)
-                q_que.append(node2.left)
-            elif node1.left != None or node2.left != None:
-                return False
-
-            if node1.right != None and node2.right != None:
-                p_que.append(node1.right)
-                q_que.append(node2.right)
-            elif node1.right != None or node2.right != None:
-                return False
-
-        return True
-
-
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         
