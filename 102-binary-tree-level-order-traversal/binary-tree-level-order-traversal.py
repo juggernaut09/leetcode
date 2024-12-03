2839ce61606fb14e195d190c18f82a6d40183095
Venkat Teja Ravi <venkatteja.ravi@gmail.com>
2024-12-02 20:04:20 -0600

    Time: 0 ms (100.00%) | Memory: 18.2 MB (6.79%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -6,27 +6,46 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        from collections import deque
+        # from collections import deque
 
-        if not root:
-            return []
+        # if not root:
+        #     return []
 
-        queue = deque([root])
-        res = []
-        while queue:
-            level_order = []
-            level_len = len(queue)
-            for _ in range(level_len):
-                node = queue.popleft()
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-                level_order.append(node.val)
+        # queue = deque([root])
+        # res = []
+        # while queue:
+        #     level_order = []
+        #     level_len = len(queue)
+        #     for _ in range(level_len):
+        #         node = queue.popleft()
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+        #         level_order.append(node.val)
 
-            res.append(level_order)
+        #     res.append(level_order)
                 
-        return res
+        # return res
+
+
+        result = []
+        def dfs(node, depth):
+            if not node:
+                return 
+
+            if depth == len(result):
+                result.append([])
+
+            result[depth].append(node.val)
+
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+
+
+        dfs(root, 0)
+
+        return result
 
             
 
